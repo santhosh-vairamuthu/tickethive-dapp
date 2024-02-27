9afb8fd5d658535f9cbdae2d216e7c3b88c7d1c4
santhosh <santhosh22032004@gmail.com>
2024-02-27 23:30:38 +0530

    jumbotron added in home page

--- src/Header.js
+++ src/Header.js
@@ -7,20 +7,22 @@ function Header() {
         <div className="container Header mt-2">
             <nav className="navbar sticky-top ">
                 <div className="container-fluid d-flex justify-content-between align-items-center">
-                    <a className="navbar-brand d-flex align-items-center d-lg-flex flex-row gap-1" href="#">
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a className="navbar-brand d-flex align-items-center d-lg-flex flex-row gap-2" href="#">
                         <img src="logo.png" alt="Logo" width="30" height="28" className="d-inline-block align-text-top"/>
                         <span className="fw-bold text-white">Ticket Hive</span>
                     </a>
-
-
                     <ul className="navbar-nav d-flex flex-row justify-content-center gap-5">
                         <li className="nav-item ml-3">
+                            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                             <a className="nav-link active fw-bolder" aria-current="page" href="#">Home</a>
                         </li>
                         <li className="nav-item ml-3 fw-bolder">
+                            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                             <a className="nav-link" href="#">Events</a>
                         </li>
                         <li className="nav-item ml-3 fw-bolder">
+                            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                             <a className="nav-link" href="#">Account</a>
                         </li>
                     </ul>
@@ -29,7 +31,7 @@ function Header() {
                             <button className="btn login fw-bold">Log in</button>
                         </li>
                         <li className="nav-item">
-                            <button className="btn signup">Signup <i className="bi bi-arrow-up-right-square-fill"/></button>
+                            <button className="btn signup">Signup</button>
                         </li>
                     </ul>
                 </div>
--- src/Home.js
+++ src/Home.js
@@ -5,9 +5,19 @@ function Home() {
     
     return (
         <div className="container-fluid Home">
-            {/* <div className="container-fluid banner">
-            <img src="./assets/banner.jpeg" class="img-fluid" alt="banner"/>
-            </div> */}
+            <div className="p-5 mb-4">
+                <div className="container-fluid py-5  rounded-3 HomeJumbotron">
+                    <div className="container JumbotronContent">
+                        <h1 className="display-5 fw-bold">Book your<br/>Tickets for Events !</h1>
+                        <ul className="mt-3">
+                            <li><p className="col-md-8">Safe, Secure, Reliable ticketing!</p></li>
+                            <li><p className="col-md-8">Your Ticket to live entertainment!</p></li>
+                            <li><p className="col-md-8">Tickets that you can keep for life.!</p></li>
+                        </ul>
+                        <button className="btn mt-4 EventRedirect">View More  <i className="bi bi-arrow-up-right-square-fill"/></button>
+                    </div>
+                </div>
+            </div>
         </div>
     
     );
