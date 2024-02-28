bb62e34962dc7575f8c7942a2fda860d66150be0
santhosh <santhosh22032004@gmail.com>
2024-02-28 16:48:42 +0530

    feature event card completed

--- src/Home.js
+++ src/Home.js
@@ -19,21 +19,49 @@ function Home() {
                 </div>
             </div>
 
-            <div className="px-5 mt-5 mb-4 homeViewMore">
-                <div className="container-fluid py-5  rounded-3 ">
+            <div className="px-4 mt-5 mb-4 ">
+                <div className="container-fluid p-5  rounded-5 homeViewMore">
                     <div className="container homeViewMoreContent">
-                        <h3 className="display-5 fw-bold">Featured Events</h3>
+                        <h3 className="display-5 fw-bold mt-2">Featured Events</h3>
                         <p className="col-md-8">Be sure not to miss these events!</p>
-                        <div className="row-3">
-                            <div className="card p-3 col-4 col-md-4 homeCards" style={{ width: "18rem" }}>
-                                <img src="https://placehold.co/600x400" className="card-img-top" alt="event" />
-                                <div className="card-body p-0 mt-2">
-                                    <h5 className="card-title">ARR Concert</h5>
-                                    <p className="card-text">Immerse in ARR's magical melodies for an unforgettable concert experience!</p>
-                                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                                    <a href="#" className="btn btn-primary">Get Tickets</a>
+                        <div className="row row-cols-1 row-cols-sm-1 row-cols-md-3">
+                            <div className="col">
+                                <div className="card p-2">
+                                    <img src="https://placehold.co/600x400" className="card-img-top" alt="event" />
+                                    <div className="card-body p-0 mt-2">
+                                        <h5 className="card-title text-white">ARR Concert</h5>
+                                        <p className="card-text text-white">Immerse in ARR's magical melodies for an unforgettable concert experience!</p>
+                                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                                        <a href="#" className="btn getTicket">Get Tickets</a>
+                                    </div>
                                 </div>
                             </div>
+
+                            <div className="col">
+                                <div className="card p-2">
+                                    <img src="https://placehold.co/600x400" className="card-img-top" alt="event" />
+                                    <div className="card-body p-0 mt-2">
+                                        <h5 className="card-title text-white">ARR Concert</h5>
+                                        <p className="card-text text-white">Immerse in ARR's magical melodies for an unforgettable concert experience!</p>
+                                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                                        <a href="#" className="btn getTicket">Get Tickets</a>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className="col">
+                                <div className="card p-2">
+                                    <img src="https://placehold.co/600x400" className="card-img-top" alt="event" />
+                                    <div className="card-body p-0 mt-2">
+                                        <h5 className="card-title text-white">ARR Concert</h5>
+                                        <p className="card-text text-white">Immerse in ARR's magical melodies for an unforgettable concert experience!</p>
+                                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                                        <a href="#" className="btn getTicket">Get Tickets</a>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
